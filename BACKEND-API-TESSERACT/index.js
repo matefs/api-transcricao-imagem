7ed50c3565b91a5eac697a13265bef1293e8fa9e
Mateus Schverz <mate_fs@hotmail.com>
2023-06-15 23:22:14 -0300

    Initial commit

---
+++ BACKEND-API-TESSERACT/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const Tesseract = require('tesseract.js');
+const cors = require('cors');
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cors()); // Adiciona o CORS à sua API
+
+
+
+app.post('/transcrever', async (req, res) => {
+  try {
+    const base64Image = req.body.image;
+    const imageBuffer = Buffer.from(base64Image, 'base64');
+
+    const result = await Tesseract.recognize(imageBuffer, 'por');
+    const text = result.data.text;
+
+    res.json({ text });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Ocorreu um erro ao transcrever a imagem.' });
+  }
+});
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`API rodando em http://localhost:${port}`);
+});
