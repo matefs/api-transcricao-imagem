4236e1b072c34d088a1f717d4dc7ae6cb978c8a7
Mateus Schverz <matefs8569@gmail.com>
2024-09-26 11:21:26 -0300

    fix(express): increased express mb limit

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const Tesseract = require('tesseract.js');
 const cors = require('cors');
 
-
-
 const app = express();
-app.use(express.json());
+
+app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 
 app.use(express.static('public'));
